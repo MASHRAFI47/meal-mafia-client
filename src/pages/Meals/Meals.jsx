4fb4b586032b19cda7b966f865d40880ec525edf
MASHRAFI47 <mbngms@gmail.com>
2024-08-30 22:29:06 +0600

    get all meals done

--- src/pages/Meals/Meals.jsx
+++ src/pages/Meals/Meals.jsx
@@ -1,12 +1,30 @@
 import queryString from 'query-string';
 import { useNavigate, useSearchParams } from "react-router-dom"
 import useRole from '../../hooks/useRole';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosCommon from '../../hooks/useAxiosCommon';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 
 const Meals = () => {
+    const axiosCommon = useAxiosCommon();
     const [role] = useRole();
     console.log(role)
     const navigate = useNavigate();
     const [params, setParams] = useSearchParams();
+
+
+    const { data: meals, isLoading } = useQuery({
+        queryKey: ['meals'],
+        queryFn: async () => {
+            const { data } = await axiosCommon.get("/meals")
+            return data
+        }
+    })
+
+    if(isLoading) return <LoadingSpinner />
+
+    console.log(meals)
+
 
     const handleClick = () => {
         let currentQuery = { category: "hi" }
