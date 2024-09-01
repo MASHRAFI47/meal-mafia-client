23128fd4545e458daf4c05a2770a17d67f96a993
MASHRAFI47 <mbngms@gmail.com>
2024-09-01 12:36:09 +0600

    all meals data in admin page found

--- src/pages/Dashboard/Admin/AllMeals.jsx
+++ src/pages/Dashboard/Admin/AllMeals.jsx
@@ -1,5 +1,18 @@
+import { useQuery } from "@tanstack/react-query"
+import useAxiosCommon from "../../../hooks/useAxiosCommon"
 
 const AllMeals = () => {
+    const axiosCommon = useAxiosCommon();
+    
+    const {data: meals, isLoading} = useQuery({
+        queryKey: ['meals'],
+        queryFn: async() => {
+            const {data} = await axiosCommon.get("/meals")
+            return data
+        }
+    })
+
+
     return (
         <div>AllMeals</div>
     )
