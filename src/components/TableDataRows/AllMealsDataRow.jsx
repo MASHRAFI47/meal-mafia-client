908efaaa5237ea6f418319b2f3777ed7e2060f18
MASHRAFI47 <mbngms@gmail.com>
2024-09-01 12:56:12 +0600

    all meals data displayed and buttons added

---
+++ src/components/TableDataRows/AllMealsDataRow.jsx
@@ -0,0 +1,40 @@
+import { FaRegEdit } from "react-icons/fa";
+import { FaRegTrashCan } from "react-icons/fa6";
+
+
+
+const AllMealsRow = ({ meal }) => {
+    console.log(meal)
+    return (
+        <tr>
+            <td>
+                <div className="flex items-center gap-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            <img
+                                src={meal?.image}
+                                alt="Avatar Tailwind CSS Component" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{meal?.title}</div>
+                        <div className="text-sm opacity-50">{meal?.likes} likes</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {meal?.adminName}
+            </td>
+            <td>
+                {meal?.reviews}
+            </td>
+            <th className="flex gap-2 justify-center">
+                <button className="btn btn-success text-white p-2 btn-md"><FaRegEdit size={20} /> </button>
+                <button className="btn btn-error text-white p-2 btn-md"><FaRegTrashCan size={20} /> </button>
+                <button className="btn btn-md btn-primary">View</button>
+            </th>
+        </tr>
+    )
+}
+
+export default AllMealsRow
--- src/pages/Dashboard/Admin/AllMeals.jsx
+++ src/pages/Dashboard/Admin/AllMeals.jsx
@@ -1,20 +1,44 @@
 import { useQuery } from "@tanstack/react-query"
 import useAxiosCommon from "../../../hooks/useAxiosCommon"
+import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
+import AllMealsDataRow from "../../../components/TableDataRows/AllMealsDataRow";
 
 const AllMeals = () => {
     const axiosCommon = useAxiosCommon();
-    
-    const {data: meals, isLoading} = useQuery({
+
+    const { data: meals, isLoading } = useQuery({
         queryKey: ['meals'],
-        queryFn: async() => {
-            const {data} = await axiosCommon.get("/meals")
+        queryFn: async () => {
+            const { data } = await axiosCommon.get("/meals")
             return data
         }
     })
 
+    if (isLoading) return <LoadingSpinner />
+
 
     return (
-        <div>AllMeals</div>
+        <div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Distributor</th>
+                            <th>Reviews</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            meals?.map(meal => <AllMealsDataRow meal={meal} />)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
