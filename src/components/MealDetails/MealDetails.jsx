335079d8eab37f41b86154accace028b04c061ae
MASHRAFI47 <mbngms@gmail.com>
2024-09-07 21:36:43 +0600

    get single meal details added

--- src/components/MealDetails/MealDetails.jsx
+++ src/components/MealDetails/MealDetails.jsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom"
+import useAxiosCommon from "../../hooks/useAxiosCommon";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
-const MealDetails = ({id}) => {
+const MealDetails = ({ id }) => {
+    const axiosCommon = useAxiosCommon();
     const id = useParams();
+
+    const { data: meal, isLoading } = useQuery({
+        queryKey: ['meal', user?.email],
+        queryFn: async () => {
+            const { data } = await axiosCommon.get(`/meal/${id}`)
+            return data
+        }
+    })
+
+    if(isLoading) return <LoadingSpinner />
+
     return (
         <div>MealDetails</div>
     )
